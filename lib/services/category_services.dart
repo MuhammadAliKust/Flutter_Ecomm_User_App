88262681e793f8ac0732acb2edea71a8b78e91d3
Muhammad Ali Nizami <kustalics@gmail.com>
2021-12-27 15:37:32 +0500

    Working on Services

--- lib/services/category_services.dart
+++ lib/services/category_services.dart
@@ -1,3 +1,40 @@
-class CategoryServices{
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:ecom_user_side_app/models/category_model.dart';
+
+class CategoryServices {
+  ///Create Category
+  Future createCategory(CategoryModel categoryModel) async {
+    return await FirebaseFirestore.instance
+        .collection('categoryCollection')
+        .add(categoryModel.toJson());
+  }
+
+  ///Update Category
+  Future updateCategory(CategoryModel categoryModel) async {
+    return await FirebaseFirestore.instance
+        .collection('collectionPath')
+        .doc(categoryModel.categoryId)
+        .update({
+      'categoryImage': categoryModel.categoryImage,
+      'categoryName': categoryModel.categoryName
+    });
+  }
+
+  ///Delete Category
+  Future deleteCategory(String categoryID) async {
+    return await FirebaseFirestore.instance
+        .collection('categoryCollection')
+        .doc(categoryID)
+        .delete();
+  }
+
   ///Get All Categories
+  Stream<List<CategoryModel>> streamCategories() {
+    return FirebaseFirestore.instance
+        .collection('categoryCollection')
+        .snapshots()
+        .map((list) => list.docs
+            .map((singleDoc) => CategoryModel.fromJson(singleDoc.data()))
+            .toList());
+  }
 }
--- lib/services/product_services.dart
+++ lib/services/product_services.dart
@@ -1,4 +1,40 @@
-class ProductServices{
-  ///Get All Products
-  ///Get Product Based upon Rating
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:ecom_user_side_app/models/category_model.dart';
+import 'package:ecom_user_side_app/models/product_model.dart';
+
+class ProductServices {
+  ///Create Product
+  Future createProduct(ProductModel categoryModel) async {
+    return await FirebaseFirestore.instance
+        .collection('productCollection')
+        .add(categoryModel.toJson());
+  }
+
+  ///Update Category
+  Future updateProduct(ProductModel categoryModel) async {
+    return await FirebaseFirestore.instance
+        .collection('productCollection')
+        .doc(categoryModel.categoryId)
+        .update({
+      //TODO: Write update product fields till tomorrow
+    });
+  }
+
+  ///Delete Category
+  Future deleteProduct(String categoryID) async {
+    return await FirebaseFirestore.instance
+        .collection('productCollection')
+        .doc(categoryID)
+        .delete();
+  }
+
+  ///Get All Categories
+  Stream<List<ProductModel>> streamProduct() {
+    return FirebaseFirestore.instance
+        .collection('productCollection')
+        .snapshots()
+        .map((list) => list.docs
+            .map((singleDoc) => ProductModel.fromJson(singleDoc.data()))
+            .toList());
+  }
 }
